6560f1c1e4fe03748de812f25078e17c4ba0e0bc
Boris <boris.bortnitskii@yandex.by>
2021-04-10 00:10:56 +0300

    v1.1

---
+++ assets/js/slider-why.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.slider-why').slick({
+		arrows: false,
+		dots: false,
+		slidesToShow: 1,
+		autoplay: true,
+		speed: 500,
+		autoplaySpeed: 5000,
+	});
+});
+
---
+++ assets/js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows: false,
+		dots: true,
+		slidesToShow: 1,
+		autoplay: true,
+		speed: 500,
+		autoplaySpeed: 5000,
+	});
+});
+
